0e2f7bf6d14266e3ef5966442f844432e20d4533
Mathias <villegasmathias@gmail.com>
2024-11-14 22:41:38 -0300

    resumen de quienes somos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,13 +33,14 @@ export default function Home() {
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
 
-      <SectionTitle
+      {/* <SectionTitle
         preTitle="Quienes somos"
         title="Introducción"
       >
-        Nuestro servicio se basa en más de 15 años de experiencia en el sector, trabajando en roles claves como ayudante, apuntador, operador de END, supervisor, planificador, inspector y coordinador de control de calidad, producción, seguridad y salud en obras. He tenido el privilegio de colaborar con empresas reconocidas a nivel nacional e internacional, siempre enfocado en la seguridad y salud en el trabajo además del buen arte de la calidad. Esta prioridad no solo busca proteger a los empleados, sino que también está alinieada con el compromiso de maximizar la producción y el crecimiento sostenible de la empresa. A lo largo de mi trayectoria, eh estado comprometido con el estudio y la aplicación de la ingeniería industrial, la gestión de calidad y la mejora continua en las organizaciones. Esta sólida base de conocimientos y experiencias nos permite ofrecer un servicio de Consultoría excepcional, adaptado a las necesidades específicas de las empresas en las instrstrias del gas, petróleo y minería.
-      </SectionTitle>
+        Nuestro servicio se basa en más de 15 años de experiencia en el sector, trabajando en roles claves como ayudante, apuntador, operador de END, supervisor, planificador, inspector y coordinador de control de calidad, producción, seguridad y salud en obras. He tenido el privilegio de colaborar con empresas reconocidas a nivel nacional e internacional, siempre enfocado en la seguridad y salud en el trabajo además del buen arte de la calidad. Esta prioridad no solo busca proteger a los empleados, sino que también está alinieada con el compromiso de maximizar la producción y el crecimiento sostenible de la empresa. A lo largo de mi trayectoria, eh estado comprometido con el estudio y la aplicación de la ingeniería industrial, la gestión de calidad y la mejora continua en las organizaciones. Esta sólida base de conocimientos y experiencias nos permite ofrecer un servicio de Consultoría excepcional, adaptado a las necesidades específicas de las empresas en las industrias del gas, petróleo y minería.
+      </SectionTitle> */}
       <SectionTitle
+        preTitle="Quienes somos"
         title="Visión"
       >
         Nuestra visión es revolucionar la conexión entre personas y empresas en las industrias del gas, petróleo y minería, donde el talento florece y la vanguardia, la seguridad y la calidad son nustrso estandartes para un futuro sostenible.
@@ -47,7 +48,7 @@ export default function Home() {
       <SectionTitle
         title="Misión"
       >
-       Nuestra misión es conectar talentos humanos con oportunidades laborales en las industrias del gas, petróleo y minería, abiertos a la expansión a otros rubros, brindando servicios de capacitación integral, entrevistas de candidatos y búsqueda de todos los niveles de experiencia, entre otros, que pararan al recurso humano para el ingreso a su futuro profesional, siendo nosotros el filtro justo. Nos comprometemos a hacer crecer los valores y retener talentos, ofreciendo asesoría personalizada y soluciones innoovadoras que garantizan el éxito de tu empresa en las áreas de recursos humanos y relaciones laborales, así como en producción, seguridad y calidad, alcanzando su máximo punto de excelencia. 
+       Nuestra misión es conectar talentos con oportunidades en las distintas industrias, con capacitaciones, entrevistas y búsqueda de todos los niveles de experiencia, que pararan al recurso para el ingreso a su futuro profesional, siendo nosotros el filtro. Nos comprometemos a hacer crecer los valores y retener talentos, ofreciendo asesoría, soluciones innovadoras que garantizan el éxito de tu empresa y relaciones laborales, así como en producción, seguridad y calidad.
       </SectionTitle>
 
       {/* <Video videoId="fZ0D0cnR88E" />
